fb3a7aec585d149b3b16b57fbc4ae98a8e74751b
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-01-25 11:03:22 +0200

    Fix js linters problem

--- js.js
+++ js.js
@@ -2,15 +2,16 @@ const burger = document.querySelector('#burger-container');
 const nav = document.querySelector('nav > ul');
 
 burger.addEventListener('click', () => {
-    nav.classList.toggle('slide-left');
+  nav.classList.toggle('slide-left');
 });
 
-const close = document.querySelector('#svgClose');
-close.addEventListener('click', () => {
-    nav.classList.toggle('slide-left');
+const closeBtn = document.querySelector('#svgClose');
+closeBtn.addEventListener('click', () => {
+  nav.classList.toggle('slide-left');
 });
 
 const navLinks = document.querySelectorAll('nav>ul>li>a');
+
 navLinks.forEach((link) => link.addEventListener('click', () => {
-    nav.classList.remove('slide-left');
+  nav.classList.remove('slide-left');
 }));
