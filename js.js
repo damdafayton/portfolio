9dc8707955380c576c6f1e86cbb09710acf1cdcf
backtofayton <yucelolcay@gmail.com>
2022-01-25 10:18:32 +0200

    Fix Linter

--- js.js
+++ js.js
@@ -1,16 +1,16 @@
-const burger = document.querySelector('#burger-container')
-const nav = document.querySelector('nav > ul')
+const burger = document.querySelector('#burger-container');
+const nav = document.querySelector('nav > ul');
 
 burger.addEventListener('click', () => {
-    nav.classList.toggle('slide-left')
-})
+    nav.classList.toggle('slide-left');
+});
 
-const close = document.querySelector('#svgClose')
+const close = document.querySelector('#svgClose');
 close.addEventListener('click', () => {
-    nav.classList.toggle('slide-left')
-})
+    nav.classList.toggle('slide-left');
+});
 
-const navLinks = document.querySelectorAll('nav>ul>li>a')
-navLinks.forEach(link => link.addEventListener('click', () => {
-    nav.classList.remove('slide-left')
-}))
+const navLinks = document.querySelectorAll('nav>ul>li>a');
+navLinks.forEach((link) => link.addEventListener('click', () => {
+    nav.classList.remove('slide-left');
+}));
