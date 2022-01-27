696400a95c5195fd452fbc2b970d9f005f14f1be
backtofayton <yucelolcay@gmail.com>
2022-01-27 15:34:26 +0200

    Fix eslint

--- js/contact-form.js
+++ js/contact-form.js
@@ -23,7 +23,15 @@ closeButtons.forEach((button) => {
 });
 
 // LOCAL STRORAGE
-document.addEventListener('DOMContentLoaded', (event) => {
+function saveToLocalStorage() {
+  const formName = contactForm.elements.name.value;
+  const formEmail = contactForm.elements.email.value;
+  const formText = contactForm.elements.message.value;
+  const formData = { formName, formEmail, formText };
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
   const formData = JSON.parse(localStorage.getItem('formData'));
   if (formData) {
     contactForm.elements.name.value = formData.formName;
@@ -33,15 +41,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 Array.from(contactForm.elements).forEach((element) => {
-  if (element.tagName == 'INPUT' || element.tagName == 'textarea') {
+  if (element.tagName === 'INPUT' || element.tagName === 'textarea') {
     element.addEventListener('input', saveToLocalStorage);
   }
 });
-
-function saveToLocalStorage() {
-  const formName = contactForm.elements.name.value;
-  const formEmail = contactForm.elements.email.value;
-  const formText = contactForm.elements.message.value;
-  const formData = { formName, formEmail, formText };
-  localStorage.setItem('formData', JSON.stringify(formData));
-}
