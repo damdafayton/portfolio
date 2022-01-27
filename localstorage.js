64055bd1ddcda399bdd60a6711f01025affd7367
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-01-27 12:22:53 +0200

    fix linter errors

--- localstorage.js
+++ localstorage.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const formData = JSON.parse(localStorage.getItem('formData'))
-    if (formData) {
-        contactForm.elements.name.value = formData.formName
-        contactForm.elements.email.value = formData.formEmail
-        contactForm.elements.message.value = formData.formText
-    }
+  const formData = JSON.parse(localStorage.getItem('formData'));
+  if (formData) {
+    contactForm.elements.name.value = formData.formName;
+    contactForm.elements.email.value = formData.formEmail;
+    contactForm.elements.message.value = formData.formText;
+  }
 });
 
-Array.from(contactForm.elements).forEach(element => {
-    if (element.tagName == 'INPUT' || element.tagName == 'textarea') {
-        element.addEventListener('input', saveToLocalStorage)
-    }
-})
+Array.from(contactForm.elements).forEach((element) => {
+  if (element.tagName == 'INPUT' || element.tagName == 'textarea') {
+    element.addEventListener('input', saveToLocalStorage);
+  }
+});
 
 function saveToLocalStorage() {
-    let formName = contactForm.elements.name.value
-    let formEmail = contactForm.elements.email.value
-    let formText = contactForm.elements.message.value
-    let formData = { formName, formEmail, formText }
-    localStorage.setItem('formData', JSON.stringify(formData))
+  const formName = contactForm.elements.name.value;
+  const formEmail = contactForm.elements.email.value;
+  const formText = contactForm.elements.message.value;
+  const formData = { formName, formEmail, formText };
+  localStorage.setItem('formData', JSON.stringify(formData));
 }
